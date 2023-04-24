df00e3fd6f164adbd80f4fe5f5816a1ee6eea786
xzzz7 <xng1@babson.edu>
2023-04-24 14:10:04 -0400

    tele bot replying features

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask
+from flask import request
+from flask import Response
+import requests
+ 
+TOKEN = "6205125004:AAFK5Gqn5AHJPGQRR5KW-7yPsGFJLrspPow"
+app = Flask(__name__)
+ 
+def parse_message(message):
+    print("message-->",message)
+    chat_id = message['message']['chat']['id']
+    txt = message['message']['text']
+    print("chat_id-->", chat_id)
+    print("txt-->", txt)
+    return chat_id,txt
+ 
+def tel_send_message(chat_id, text):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    payload = {
+                'chat_id': chat_id,
+                'text': text
+                }
+   
+    r = requests.post(url,json=payload)
+    return r
+
+def tel_send_image(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+    payload = {
+        'chat_id': chat_id,
+        'photo': "https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
+        'caption': "This is a sample image"
+    }
+ 
+    r = requests.post(url, json=payload)
+    return r
+
+def tel_send_inlineurl(chat_id):
+    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+ 
+    payload = {
+        'chat_id': chat_id,
+        'text': "Which link would you like to visit?",
+        'reply_markup': {
+            "inline_keyboard": [
+                [
+                    {"text": "google", "url": "http://www.google.com/"},
+                    {"text": "youtube", "url": "http://www.youtube.com/"}
+                ]
+            ]
+        }
+    }
+ 
+    r = requests.post(url, json=payload)
+ 
+    return r
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        msg = request.get_json()
+       
+        chat_id,txt = parse_message(msg)
+        if txt == "hi":
+            tel_send_message(chat_id,"Hello!!")
+        elif txt == "image":
+            tel_send_image(chat_id)
+        elif txt == "inlineurl":
+            tel_send_inlineurl(chat_id)
+        else:
+            tel_send_message(chat_id,'from webhook')
+       
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
+ 
+if __name__ == '__main__':
+   app.run(debug=True)
